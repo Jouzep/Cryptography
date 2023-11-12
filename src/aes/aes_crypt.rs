e43ef9ddd687a58734aee390cc07ebfd58421e07
jouzep <josephyu31@gmail.com>
2023-11-12 01:04:11 +0100

    feat(AES): generating all the keys

--- src/aes/aes_crypt.rs
+++ src/aes/aes_crypt.rs
@@ -7,8 +7,15 @@ use crate::aes::aes_key_struct::*;
 
 pub fn aes_crypt(_a: &[u8], key: String) -> Vec<u8> {
     let start_key = Key::new(key);
-
-    let _second_key = Key::new_w_s_box(&start_key);
+    let mut expanded_keys: Vec<Key> = Vec::new();
+    for index in 0..RCON.len() {
+        if let Some(test ) = expanded_keys.last()
+        {
+            expanded_keys.push(Key::new_w_s_box(test, index));
+        } else {
+            expanded_keys.push(Key::new_w_s_box(&start_key, index));
+        }
+    }
     return Vec::new();
 }
 
