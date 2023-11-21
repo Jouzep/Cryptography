b99e79c6dd90a507b0f38192f13f503afc63f136
BRR steven <steven.dong@epitech.eu>
2023-11-21 22:37:24 +0100

    fix(error handling)

--- src/error_handler.rs
+++ src/error_handler.rs
@@ -13,24 +13,53 @@ pub fn get_message() -> String {
     result
 }
 
-fn xor_aes_error(algo: &str, message: &str, block: bool, key: &str, content: &String) -> bool {
+fn xor_aes_error(algo: &str, message: &String, block: bool, mode: &str, content: &String) -> Result<(), &'static str> {
     let message_list = vec!["-c", "-d"];
 
-    if algo == "-xor" && (!block || !message_list.contains(&message) || algo == "-xor" && content.len() != key.len()) {
-        return true;
+    if !message_list.contains(&mode) {
+        println!("Error");
+        return Err("Not a good crypt mode");
     }
-    false
+
+    if (algo == "-xor" || block) && message.len() != content.len() {
+        println!("error");
+        return Err("Block mode error");
+    }
+
+    Ok(())
+}
+
+fn rsa_error<'a>(args: &'a [String]) -> Result<(), &'static str> {
+    let message_list = vec!["-c", "-d", "-g"];
+
+    if !message_list.contains(&args[2].as_str()) {
+        println!("Error");
+        return Err("Wrong rsa mode")
+    }
+
+    Ok(())
 }
 
 pub fn error_handler<'a>(args: &'a [String]) -> Result<(&'a [String], String), &'static str> {
-    let buffer: String = if args[1] == "-rsa" && args[2] == "-g" { "".to_string() } else {  get_message()};
+    let buffer: String = if args[1] == "-rsa" && args[2] == "-g" {
+        "".to_string()
+    } else {
+        get_message()
+    };
 
-    if let [_, algo, message, block, key] = args {
-        if xor_aes_error(algo, &message, block == "-b", key, &buffer) {
-            return Err("Error in XOR or AES validation");
-        }
+    if args.len() > 4 {
+        return match args[1].as_str() {
+            "-rsa" => rsa_error(args).map(|()| (args, buffer)),
+            "-aes" | "-xor" => {
+                let block = args[3].parse().unwrap_or(false);
+                xor_aes_error(&args[1].as_str(), &buffer, block, &args[2].as_str(), &args[4]).map(|()| (args, buffer))
+            }
+            _ => Err("Error in crypt flag"),
+        };
     } else {
         println!("Not sufficient args");
+        // Decide what to return in this case
+        // Returning an Err for now
+        Err("Not sufficient args")
     }
-    Ok((args, buffer))
 }
