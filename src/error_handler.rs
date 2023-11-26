d131194a41879488da8a27405c7aa765272a258b
BRR steven <steven.dong@epitech.eu>
2023-11-26 19:50:40 +0100

    fix(error handling)

--- src/error_handler.rs
+++ src/error_handler.rs
@@ -24,7 +24,7 @@ fn xor_aes_error(algo: &str, message: &String, block: bool, mode: &str, content:
         return Err("Not a good crypt mode");
     }
 
-    if (algo == "-xor" || block) && message.len() != content.len() {
+    if block && (message.len() != content.len()) {
         println!("error");
         return Err("Block mode error");
     }
@@ -36,7 +36,7 @@ fn is_hexadecimal(s: &str) -> bool {
     s.chars().all(|c| c.is_digit(16))
 }
 
-fn rsa_error<'a>(args: &'a [String]) -> Result<(), &'static str> {
+fn rsa_error<'a>(args: &'a [String], buffer:&String) -> Result<(), &'static str> {
     let message_list = vec!["-c", "-d", "-g"];
 
     if args.len() < 4 || args.len() > 5 {
@@ -48,6 +48,7 @@ fn rsa_error<'a>(args: &'a [String]) -> Result<(), &'static str> {
         return Err("Wrong rsa mode")
     }
 
+
     if args[2] == "-g" {
         let p = convert_little_endian(args[3].clone());
         let q = convert_little_endian(args[4].clone());
@@ -55,19 +56,26 @@ fn rsa_error<'a>(args: &'a [String]) -> Result<(), &'static str> {
         if !is_hexadecimal(&p) || !is_hexadecimal(&q) {
             return Err("Invalid hexadecimal representation for p or q");
         }
+    } else {
+        if args.len() != 4 {
+            return Err("Wrong nb of args");
+        }
+        let key: Vec<&str> = args[2].split("-").collect();
+        if buffer.len() < key[1].len() {
+            return Err("Message can't be smaller than p or q")
+        }
     }
     Ok(())
 }
 
 fn pgp_error<'a>(args: &'a [String]) -> Result<(), &'static str> {
-    let message_list = vec!["-c", "-d", "-g"];
+    let message_list = vec!["-c", "-d"];
 
 
     if args.len() != 4 {
         return Err("Wrong nb of args");
     }
-
-    if message_list.contains(&args[2].as_str()) {
+    if !message_list.contains(&args[2].as_str()) {
         return Err("Wrong pgp flag");
     }
     Ok(())
@@ -80,12 +88,12 @@ pub fn error_handler<'a>(args: &'a [String]) -> Result<(&'a [String], String), &
         get_message()
     };
 
-    if args.len() >= 4 {
+    if args.len() >= 4 && args.len() <= 5 {
         return match args[1].as_str() {
             "-pgp" => pgp_error(args).map(|()| (args, buffer)),
-            "-rsa" => rsa_error(args).map(|()| (args, buffer)),
+            "-rsa" => rsa_error(args, &buffer).map(|()| (args, buffer)),
             "-aes" | "-xor" => {
-                let block = args[3].parse().unwrap_or(false);
+                let block = args.contains(&"-b".to_string());
                 xor_aes_error(&args[1].as_str(), &buffer, block, &args[2].as_str(), &args[4]).map(|()| (args, buffer))
             }
             _ => Err("Error in crypt flag"),
--- src/my_pgp.rs
+++ src/my_pgp.rs
@@ -15,14 +15,14 @@ fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
 fn run_xor_aes(mut args: Vec<String>, content: String) {
     // let key = hex::decode(args[4].to_string()).unwrap();
     let key = hex::decode(&args[4]).expect("Failed to decode hexadecimal string");
-    let decoded_result = hex::decode(content).expect("Failed to decode hexadecimal string");
+    let decoded_result = hex::decode(&content).expect("Failed to decode hexadecimal string");
     // let byte_slice: &[u8] = &decoded_result;
     //
     // println!("{:?}", byte_slice);
     let result = match (args[1].as_str(), args[2].as_str()) {
         ("-xor", "-c" | "-d") => xor(&decoded_result, &key),
-        ("-aes", "-c") => aes_module(decoded_result, args.remove(4), "cipher"),
-        ("-aes", "-d") => aes_module(decoded_result, args.remove(4), "decipher"),
+        ("-aes", "-c") => aes_module(decoded_result, args.remove(4), "cipher", content.len()),
+        ("-aes", "-d") => aes_module(decoded_result, args.remove(4), "decipher", content.len()),
         _ => Vec::new(),
     };
     println!("{}", hex::encode(result));
